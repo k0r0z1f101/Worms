017f7179989b5636395191811966ca6d9caa683a
wa <9841584@bdeb.qc.ca>
2022-06-30 04:15:19 -0400

    generatelevel base is done, instantiate placeholders worms

--- Assets/Scripts/GenerateLevel.cs
+++ Assets/Scripts/GenerateLevel.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Animations;
 
 public class GenerateLevel : MonoBehaviour
 {
-    private GameObject blockPrefab;
+    [SerializeField] private int _depth = 3;
+    [SerializeField] private int _height = 50;
+    [SerializeField] private int _width = 70;
+    [SerializeField] private int _holesQty = 30;
+    [SerializeField] private float _maxDistanceFromHole = 5.29f;
+    private List<Vector2> _holes = new List<Vector2>();
+    private GameObject _blockPrefab;
     
     //Load level base (water and land)
     
@@ -14,42 +21,58 @@ public class GenerateLevel : MonoBehaviour
     
     void Awake()
     {
-        blockPrefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Destructible.prefab", typeof(GameObject));
-        print(blockPrefab);
-        SpawnWholeWall();
+        _holes = new List<Vector2>();
+        _blockPrefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Destructible.prefab", typeof(GameObject));
+        DrawLevel();
     }
 
-    void SpawnWholeWall()
+    void SpawnWorm(ref Vector2 pos)
     {
-        int depth = 1;
-        int height = 7;
-        int width = 10;
+        GameObject newWorm = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Worm.prefab", typeof(GameObject));
+        Instantiate(newWorm, new Vector3(pos.x, pos.y, 1), new Quaternion(0, 1 ,0 , 1));
+    }
+
+    void DrawLevel()
+    {
+        _holes = new List<Vector2>();
         
-        float emptySpace = 0.5f;
-        int holes = 4;
+        int wormsQty = 8;
+        int wormsCtr = 0;
 
-        float holeSize = (height * width * emptySpace) / holes;
-        List<Vector2> occupiedByHole = new List<Vector2>();
-
-        for (int h = 0; h < height; ++h)
+        for (int h = 0; h < _holesQty; ++h)
         {
-            for (int w = 0; w < width; ++w)
+            Vector2 newHole = new Vector2();
+            newHole.x = Random.Range(1, _width - 1);
+            newHole.y = Random.Range(1, _height - 1);
+            _holes.Add(newHole);
+
+            if (h * 0.125f >= wormsCtr)
             {
-                for (int d = 0; d < depth; ++d)
-                {
-                    GameObject newBlock = blockPrefab;
-                    Instantiate(newBlock, new Vector3(w, h, d), Quaternion.identity);
-                }
+                SpawnWorm(ref newHole);
+                ++wormsCtr;
             }
         }
 
-        for (int h = 0; h < holes; ++h)
+        for (int h = 0; h < _height; ++h)
         {
-            int xMin = -1;
-            int yMin = -1;
-            int xMax = -1;
-            int yMax = -1;
+            for (int w = 0; w < _width; ++w)
+            {
+                for (int d = 0; d < _depth; ++d)
+                {
+                    bool inHole = false;
+                    for (int ho = 0; ho < _holes.Count; ++ho)
+                    {
+                        Vector2 distanceFromHole = new Vector2(w, h) - _holes[ho];
+                        inHole = distanceFromHole.magnitude < _maxDistanceFromHole ? true : inHole;
+                    }
 
+                    if (!inHole)
+                    {
+                        GameObject newBlock = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        newBlock.transform.position = new Vector3(w, h, d);
+                    }
+                }
+            }
         }
     }
 }
